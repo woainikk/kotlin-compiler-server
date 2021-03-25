ed1870ccff04898ca1d09f0194013ceb341d0852
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2021-03-25 16:51:43 +0300

    add 1.5.0-M2-519

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,11 @@ rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
 include(":common")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
+    }
+}
